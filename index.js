7517b5720f831a1abb88e5a340ab74ad2216a41e
verdantkyber <kingjalexa@gmail.com>
2019-06-08 05:31:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,20 +11,51 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
-}
+   var newItem = {
+   itemName : item,
+   itemPrice : (Math.floor(Math.random() * 101))
+   };
+   cart.push(newItem);
+ return `${newItem.itemName} has been added to your cart.`;
+ }
 
 function viewCart() {
   // write your code here
+  if (cart.length === 0){
+    return `Your shopping cart is empty.`
+  }
+  else if (cart.length === 1{
+    return `in your cart, you have ${cart.itemName
+    let output = `In your cart, you have `;
+  for (let i = 0; i < cart.length; i++){
+    output += `${cart[i].itemName} at ${cart[i].itemPrice}, `;
+  }
+  output += `and ${cart[cart.length].itemName} at ${cart[cart.length].itemPrice}.`;
+  return output;
+  }
 }
 
 function total() {
   // write your code here
+  let output = 0
+  for (let i = 0; i < cart.length; i++){
+    output += cart[i].itemPrice;
+  }
+  return output
 }
 
 function removeFromCart(item) {
+  
   // write your code here
+  cart.forEach = function (item){
+    if 
+  }
 }
 
 function placeOrder(cardNumber) {
   // write your code here
+  if (cardNumber){
+    console.log( `Sorry, we don't have a credit card on file for you.`);
+  }
+  
 }
